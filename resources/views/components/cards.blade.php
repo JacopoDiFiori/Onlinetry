5484ad42a3f7d7e6533fb46fb2610d2402428a98
Jacopo Di Fiori <[jacopodifiori14@gmail.com]>
2024-07-10 17:37:13 +0200

    edit1

---
+++ resources/views/components/cards.blade.php
@@ -0,0 +1,33 @@
+
+<div class="container">
+    <div class="card__container ">
+        <article class="card__article">
+            <img src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0f/ba/29/5c/img-worlds-of-adventure.jpg?w=1200&h=1200&s=1" alt="image" class="card__img">
+            
+            <div class="card__data">
+                <span class="card__description">Vancouver</span>
+                <h2 class="card__title">the great path</h2>
+                <a href="#" class="card__button">Read More</a>
+            </div>
+        </article>
+
+        <article class="card__article">
+            <img src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0f/ba/29/5c/img-worlds-of-adventure.jpg?w=1200&h=1200&s=1" alt="image" class="card__img">
+            <div class="card__data">
+                <span class="card__description">a</span>
+                <h2 class="card__title">b</h2>
+                <a href="#" class="card__button">Read More</a>
+            </div>
+        </article>
+
+        <article class="card__article">
+            <img src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0f/ba/29/5c/img-worlds-of-adventure.jpg?w=1200&h=1200&s=1" alt="image" class="card__img">
+
+            <div class="card__data">
+                <span class="card__description">a</span>
+                <h2 class="card__title">b</h2>
+                <a href="#" class="card__button">Read More</a>
+            </div>
+        </article>
+    </div>
+</div>
